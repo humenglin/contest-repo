6eeb0627822c417404c5490940861419f8675b64
humenglin <humenglin@zyb.com.cn>
2020-12-31 17:11:07 +0800

    test:计算游戏中10个宝箱的全部神秘数字

--- src/test/java/TreasureBoxGameTest.java
+++ src/test/java/TreasureBoxGameTest.java
@@ -24,4 +24,22 @@ public class TreasureBoxGameTest {
         assertFalse(gameStatus);
     }
 
+    @Test
+    public void should_return_game_status_is_open_when_game_is_over_given_10_right_mysterious_no() {
+        TreasureBoxGame treasureBoxGame = new TreasureBoxGame();
+
+        treasureBoxGame.play(1, 610536);
+        treasureBoxGame.play(2, 167626);
+        treasureBoxGame.play(3, 661789);
+        treasureBoxGame.play(4, 62753);
+        treasureBoxGame.play(5, 667142);
+        treasureBoxGame.play(6, 3621134);
+        treasureBoxGame.play(7, 4479782);
+        treasureBoxGame.play(8, 58324);
+        treasureBoxGame.play(9, 197357);
+        treasureBoxGame.play(10, 613080);
+        boolean gameStatus = treasureBoxGame.getFinalStatus();
+
+        assertFalse(gameStatus);
+    }
 }
